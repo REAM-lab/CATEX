e65b2fd8f15919f40b36dff66e947da3ed78fb54
Paul Serna-Torre <psernatorre@gmail.com>
2025-12-01 15:40:17 -0800

    Fix: case-insensitive

---
+++ src/Scenarios.jl
@@ -0,0 +1,44 @@
+"""
+Scenarios module for managing scenario data in the stochastic capacity expansion problem.
+"""
+module Scenarios   
+
+# Use Julia standard libraries and third-party packages
+using NamedArrays
+
+# Use internal modules
+using ..Utils
+
+# Export variables and functions
+export Scenario, load_data
+
+"""
+Scenario represents a scenario in the stochastic capacity expansion problem.
+# Fields:
+- sc_id: ID of the scenario
+- prob: probability of the scenario
+"""
+struct Scenario
+    sc_id:: String
+    prob:: Float64
+end
+
+"""
+Load scenario data from a CSV file and return it as a NamedArray of Scenario structures.
+"""
+function load_data(inputs_dir:: String):: NamedArray{Scenario}
+
+    # Get a list of Scenario structures
+    scen = to_Structs(Scenario, inputs_dir, "scenarios.csv")
+
+    # Get a list of the scenario IDs
+    S = getfield.(scen, :sc_id)
+
+    # Transform scen into NamedArray, so we can access scenarios by their IDs
+    scen = NamedArray(scen, (S))
+
+    return scen
+
+end
+
+end # module Scenarios
---
+++ src/Utils.jl
@@ -0,0 +1,119 @@
+module Utils
+
+# Use Julia standard libraries and third-party packages
+using DataFrames, JuMP, CSV, NamedArrays
+
+# Export variables and functions
+export to_Structs, to_multidim_NamedArray, to_Df
+
+function to_Structs(structure::DataType, inputs_dir:: String, filename:: String):: Vector{structure}
+    file_dir = joinpath(inputs_dir, filename)
+    struct_names = fieldnames(structure)
+    struct_types = fieldtypes(structure)
+
+    first_csvlines = CSV.File(joinpath(inputs_dir, filename); limit=1)
+    csv_header = Tuple(propertynames(first_csvlines))
+
+    @assert csv_header == struct_names """Column names of $filename does not match the fields of the structure $structure."""
+
+    df = CSV.read(file_dir, DataFrame; types=Dict(zip(struct_names, struct_types)))
+    cols = Tuple(df[!, col] for col in names(df))
+    V = structure.(cols...)    
+    
+    return V
+end
+
+function to_multidim_NamedArray(structures:: Vector{T}, dims:: Vector{Symbol}, value:: Symbol):: NamedArray{Union{Missing, Float64}} where {T} 
+  
+    vals_in_dim = [unique(getfield.(structures, d)) for (i, d) in enumerate(dims)]
+    
+    arr = Array{Union{Missing, Float64}}(missing, length.(vals_in_dim)...)
+    arr = NamedArray(arr, vals_in_dim, dims)
+
+    for s in structures
+        arr[getfield.(Ref(s), dims)...] = getfield(s, value)
+    end
+
+    return arr
+end
+
+"""
+Rehashing and Resizing: When a Dict grows beyond its current allocated capacity, 
+it needs to be resized, which often involves rehashing all existing key-value pairs 
+and moving them to a larger memory location. This process can be computationally expensive, 
+especially with a large number of elements
+"""
+function to_stacked_Dict(data:: DataFrame, key:: String, value:: String)
+    col_key = data[!, Symbol(key)]
+    col_value = data[!, Symbol(value)]
+
+    tuples = collect(zip(col_key, col_value))
+
+    stacked_dict = Dict()
+
+    for (key, value) in tuples
+        if haskey(stacked_dict, key)
+            push!(stacked_dict[key], value)
+        else
+            stacked_dict[key] = [value]
+        end
+    end
+    
+    return stacked_dict
+end
+
+function to_Dict(data:: DataFrame, key:: Symbol, value:: Symbol)
+    return Dict(Pair.(data[:, key], data[:, value]))
+end
+
+function to_tupled_Dict(data:: DataFrame, keys:: Vector{Symbol}, value:: Symbol)
+    col_keys = Tuple.(eachrow(data[:, keys]))
+    col_value = data[:, value]
+    return Dict(Pair.(col_keys, col_value))
+end
+
+
+"""
+`to_Df(var_name:: JuMP.Containers.DenseAxisArray, header:: Vector, outputs_dir:: String, filename:: String; print_csv = true)`
+
+This function returns a csv file with the numerical solution of a JuMP variable once the optimization has been finished.
+
+## Args:
+    - var_name: variable name defined in the JuMP model, e.g., GEN, CAP.
+    - header:   a vector with headers for the dataframe. It should be consistent with the dimensions.
+                For example, if it is GEN[G, TPS], then header is [:generation_project, :timepoint, :DispatchGen_MW]
+    - dir_file: directory where the csv will be saved. It must include the name, for example: /Users/paul/Documents/CATSExpand/examples/toy_example1/dispatch.csv
+## Returns:
+    - A csv file in the specified directory.
+"""
+function to_Df(var_name:: JuMP.Containers.DenseAxisArray, header:: Vector, outputs_dir:: String, filename:: String; print_csv = true)
+    
+    dir_file = joinpath(outputs_dir, filename) # directory to save the csv file
+
+    # Transform DenseAxisArray into DataFrame.
+    # The argument 'value' is a reserved word in JuMP to get the numerical value of a variable.
+    # The argument 'var_name' is the name of the variable defined in the JuMP model. For example, GEN, CAP.
+    # The argument 'header' is a vector with the headers of the dataframe. It should be consistent with the dimensions.
+    df = DataFrame(Containers.rowtable(value, var_name; header = header))
+
+    # The following loop is to replace the columns of the df with the actual IDs.
+    # If we omit this loop, the columns will contain the entire struct, e.g., Timepoint(tp_id="tp1", period_id="p1", weight=1.0)
+    # Actually, it is useful to have the entire struct in each cell of the Dataframe, because we can access its bus_id, and then make another dataframe.
+    for h in header[begin:end-1]
+    df[!, h] = getfield.(df[!, h], h)
+    end
+
+    # Print csv file. By default, it is set to true.
+    if print_csv
+        CSV.write(dir_file, df)
+        println(" > $filename printed.")
+    end
+
+    return df # return the dataframe if needed
+end
+
+
+
+
+
+end # ends utils module
