3d55b3d3bf752e7b39db4255133535b26ba09817
Paul Serna-Torre <psernatorre@gmail.com>
2025-12-22 17:27:15 -0800

    add: solver settings and model settings as dicts to inputs in functions.

--- examples/case5/run.jl
+++ examples/case5/run.jl
@@ -16,10 +16,12 @@ main_dir = @__DIR__
 # Run stochastic capacity expansion 
 sys, mod = run_stocapex(;   main_dir = main_dir, 
                             solver = Mosek.Optimizer, 
-                            print_model = true,
-                            gen_costs = "quadratic") 
+                            solver_settings = Dict(),
+                            print_model = false,
+                            model_settings = Dict(  "gen_costs" => "quadratic", 
+                                                    "consider_shedding" => false)) 
 
-#sys = init_system(main_dir = main_dir)
+#sys = init_system(main_dir)
 
 println("Finished")
 
